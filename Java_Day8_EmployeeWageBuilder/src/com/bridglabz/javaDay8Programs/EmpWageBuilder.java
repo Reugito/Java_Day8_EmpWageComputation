56d4f9c4487936a474ad464bd62ee8e64be8b28e
Reugito <raosahebdhotreedu@gmail.com>
2021-06-30 16:15:04 +0530

    adding employee salary using switch case

--- Java_Day8_EmployeeWageBuilder/src/com/bridglabz/javaDay8Programs/EmpWageBuilder.java
+++ Java_Day8_EmployeeWageBuilder/src/com/bridglabz/javaDay8Programs/EmpWageBuilder.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class EmpWageBuilder {
 	public static final int WagePerHr = 20, FullDayHr = 8, PartThimeHr = 4;
 	public static final int isFulltime = 1, isParttime = 2;
+	public static int cnt = 0;
 	public static int Attendance() {
 		Random random = new Random();
 		int check = random.nextInt(3);
@@ -20,4 +21,21 @@ public class EmpWageBuilder {
     	int PartTimeWage = WagePerHr * PartThimeHr; 
         return PartTimeWage;
     }
+	
+	public static int usingSwitch() {
+		int check = Attendance() ;
+		cnt++;
+		switch (check) {
+			case isFulltime: 
+				System.out.println("Day"+cnt+" = "+getDailyWager());
+				return getDailyWager();
+		case isParttime:  
+				System.out.println("Day"+cnt+" = "+getPartTimeWage());
+				return getPartTimeWage();
+			default:
+				System.out.println("Day"+cnt+" = 0");
+			
+		return 0;
+		}
+	}
 }
--- Java_Day8_EmployeeWageBuilder/src/com/bridglabz/javaDay8Programs/EmpWageBuilderMain.java
+++ Java_Day8_EmployeeWageBuilder/src/com/bridglabz/javaDay8Programs/EmpWageBuilderMain.java
@@ -3,6 +3,6 @@ package com.bridglabz.javaDay8Programs;
 public class EmpWageBuilderMain {
 
 	public static void main(String[] args) {
-		EmpWageBuilder.getPartTimeWage();
+		EmpWageBuilder.usingSwitch();
 	}
 }
