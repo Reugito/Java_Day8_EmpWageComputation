57e2333dd139333adb314987756bc5af19c2e2ad
Reugito <raosahebdhotreedu@gmail.com>
2021-06-30 16:52:50 +0530

    Employee monthly wager upto a condition using classes and variables

--- Java_Day8_EmployeeWageBuilder/src/com/bridglabz/javaDay8Programs/EmpWageBuilder.java
+++ Java_Day8_EmployeeWageBuilder/src/com/bridglabz/javaDay8Programs/EmpWageBuilder.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class EmpWageBuilder {
 	public static final int WagePerHr = 20, FullDayHr = 8, PartThimeHr = 4;
 	public static final int isFulltime = 1, isParttime = 2;
-	public static int cnt = 0;
+	public static int day = 0;
 	
 	public static int Attendance() {
 		Random random = new Random();
@@ -25,16 +25,16 @@ public class EmpWageBuilder {
 	
 	public static int usingSwitch() {
 		int check = Attendance() ;
-		cnt++;
+		day++;
 		switch (check) {
 			case isFulltime: 
-				System.out.println("Day"+cnt+" = "+getDailyWager());
+				System.out.println("Day"+day+" = "+getDailyWager());
 				return getDailyWager();
 		case isParttime:  
-				System.out.println("Day"+cnt+" = "+getPartTimeWage());
+				System.out.println("Day"+day+" = "+getPartTimeWage());
 				return getPartTimeWage();
 			default:
-				System.out.println("Day"+cnt+" = 0");
+				System.out.println("Day"+day+" = 0");
 			
 		return 0;
 		}
@@ -42,10 +42,29 @@ public class EmpWageBuilder {
 	
 	public static void getMonthlyWage() {
 		
-		int TotalWage = 0;
-		for(int Day = 1; Day <= 20; Day++) {
-			TotalWage += usingSwitch();
+		int totalWage = 0;
+		for(day = 1; day <= 20; day++) {
+			totalWage += usingSwitch();
 		}
-		System.out.println("Employee Monthly wager = "+TotalWage);
+		System.out.println("Employee Monthly wager = "+totalWage);
 	}
+	
+	public static void getMonthlyWageWhileCondition() {
+		
+		int totalWage = 0, wage = 0, workingHr = 0;
+		while( day < 20 && workingHr <= 100  ) {
+			wage = usingSwitch();
+			if(wage / WagePerHr == FullDayHr) {
+				totalWage += wage;
+				workingHr += FullDayHr;
+			}
+			else if(wage / WagePerHr == PartThimeHr) {
+				totalWage += wage;
+				workingHr += PartThimeHr;
+			}
+		}
+		System.out.println("Employee Monthly wager = "+totalWage);
+	}
+		
+	
 }
